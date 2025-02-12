4876f85ff8a045fe175b9b123d67b7db7aa7866e
MohammedGouseShaik <gouse.shaik@parabola9.com>
2025-02-12 00:00:01 -0800

    code updated

--- src/app/( main )/layout.tsx
+++ src/app/( main )/layout.tsx
@@ -25,8 +25,8 @@ import {
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
+  // BreadcrumbPage,
+  // BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import {
